c5f0846860d190c478fada3ef9a1fad0bbfdfcf1
dnnstone <vastahuaman@rmgperusaff.com>
2019-11-13 18:11:38 -0500

    inicio 2da parte curso redux

---
+++ src/containers/LocationListContainers.js
@@ -0,0 +1,19 @@
+// rccp +tab, genera todo el codigo de abajo
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class LocationListContainers extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+LocationListContainers.propTypes = {
+
+};
+
+export default LocationListContainers;
